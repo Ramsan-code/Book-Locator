37b2423af2c92a333636ce2fd9f6755de5524402
thaventhiran Ramsan <ramsandota@gmail.com>
2025-11-27 12:21:40 +0530

    Create loginmodal

---
+++ src/components/auth/LoginModal.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import * as React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { LoginForm } from "./LoginForm";
+
+interface LoginModalProps {
+  children: React.ReactNode;
+}
+
+export function LoginModal({ children }: LoginModalProps) {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Sign In</DialogTitle>
+          <DialogDescription>
+            Enter your credentials to access your account.
+          </DialogDescription>
+        </DialogHeader>
+        <LoginForm onSuccess={() => setOpen(false)} />
+      </DialogContent>
+    </Dialog>
+  );
+}
