a6b5c5a2de0e4c06ae9a214f21c4dcd8a19b7072
thaventhiran Ramsan <ramsandota@gmail.com>
2025-12-05 08:20:58 +0530

    Connect Api Vercel link

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,7 +3,7 @@
 import { AuthResponse, LoginCredentials, RegisterData } from "@/types/auth";
 
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+  process.env.NEXT_PUBLIC_API_URL || "https://book-link-api.vercel.app/";
 
 // ----------------------
 // Custom Error Class
